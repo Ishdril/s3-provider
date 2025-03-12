570b013f8f37a3b09d3bf780d138d2753b39fb09
Bernat Duran <berni2015@gmail.com>
2025-03-12 14:29:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,45 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3" {
-  bucket = "tofu-demo-bucket"
+  bucket = var.s3_name
 
   tags = {
-    Name        = "My bucket"
+    Name        = var.s3_name
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.s3.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.s3.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
+}
+
+variable "s3_name" {
+  type = string
+  description = "name of the s3 bucket"
+}
+
+output "s3_policy" {
+  id = aws_s3_bucket.s3.id
+  arn = aws_s3_bucket.s3.arn
+  policy = aws_s3_bucket_policy.allow_access_from_another_account.policy
 }
